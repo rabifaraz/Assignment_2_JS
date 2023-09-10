fdb054fbbdd2a950b1474241da4bc31161043066
Rabi Faraz Abbasi <rabifaraz@gmail.com>
2023-09-10 20:00:30 +0500

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+// var userage = prompt("Enter your age");
+// if ( userage <18){
+// console.log("You are not eligible for CNIC yet!");
+// }else{
+//     console.log("Ofcourse! You are eligible for CNIC");
+// }
+
+// var usernumber = parseInt(prompt("Guess the number"));
+// if (usernumber <= 12) {
+//     console.log("Congrats");
+// }else{
+//     console.log("Guess again!");
+// }
+
